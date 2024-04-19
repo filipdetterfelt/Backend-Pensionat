fc7affe5fb93c2e1e1842836482b1507b024a77e
Filip <fillip.detterfelt@gmail.com>
2024-04-19 11:08:30 +0200

    Initial commit

---
+++ src/main/java/com/example/backendpensionat/BackendPensionatApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpensionat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPensionatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPensionatApplication.class, args);
+	}
+
+}
