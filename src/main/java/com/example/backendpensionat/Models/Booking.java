3cffc8b9cc56af78aa9ecaa814424aae1598e07d
Filip <fillip.detterfelt@gmail.com>
2024-04-23 09:34:31 +0200

    Added Booking models

--- src/main/java/com/example/backendpensionat/Models/Booking.java
+++ src/main/java/com/example/backendpensionat/Models/Booking.java
@@ -1,4 +1,27 @@
 package com.example.backendpensionat.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
+
+    @Id
+    @GeneratedValue
+
+    private Long Id;
+    private int amount;
+    private Long price;
+    private String startDate;
+    private String endDate;
+
+
+
 }
