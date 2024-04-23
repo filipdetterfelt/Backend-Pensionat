3dd5cbe97505559e66a3ec2d304cf222a7a88254
Robin Blondin <robin.blondin@protonmail.com>
2024-04-23 11:08:21 +0200

    Added room DTO

--- src/main/java/com/example/backendpensionat/DTO/RoomDTO.java
+++ src/main/java/com/example/backendpensionat/DTO/RoomDTO.java
@@ -1,4 +1,10 @@
 package com.example.backendpensionat.DTO;
 
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
 public class RoomDTO {
+    private Long id;
 }
--- src/main/java/com/example/backendpensionat/DTO/RoomDetailedDTO.java
+++ src/main/java/com/example/backendpensionat/DTO/RoomDetailedDTO.java
@@ -1,4 +1,15 @@
 package com.example.backendpensionat.DTO;
 
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Builder
 public class RoomDetailedDTO {
+    private Long id;
+    private Long roomNumber = id;
+    private Double price;
+    private int size;
+    List<BookingDTO> bookings;
 }
