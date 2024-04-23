f111ca20243f90e48a1487d7aae079a5274c8f8b
Michael Speiner <speiner.m@gmail.com>
2024-04-23 11:00:47 +0200

    Created Customer DTO

--- src/main/java/com/example/backendpensionat/DTO/CustomerDTO.java
+++ src/main/java/com/example/backendpensionat/DTO/CustomerDTO.java
@@ -1,4 +1,10 @@
 package com.example.backendpensionat.DTO;
 
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Builder
 public class CustomerDTO {
+    private Long id;
 }
