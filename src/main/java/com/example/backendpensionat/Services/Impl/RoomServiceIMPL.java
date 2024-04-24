5554cfe5e776811fd2c79e3494c59f6be7255f13
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-04-24 10:34:00 +0200

    Create conversions in IMPL

--- src/main/java/com/example/backendpensionat/Services/Impl/RoomServiceIMPL.java
+++ src/main/java/com/example/backendpensionat/Services/Impl/RoomServiceIMPL.java
@@ -1,23 +1,46 @@
 package com.example.backendpensionat.Services.Impl;
 
+import com.example.backendpensionat.DTO.BookingDTO;
 import com.example.backendpensionat.DTO.RoomDTO;
 import com.example.backendpensionat.DTO.RoomDetailedDTO;
 import com.example.backendpensionat.Models.Room;
+import com.example.backendpensionat.Repos.BookingRepo;
 import com.example.backendpensionat.Services.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class RoomServiceIMPL implements RoomService {
+
+    BookingRepo bookingRepo;
+
     @Override
     public RoomDTO roomToDTO(Room room) {
-        return null;
+        return RoomDTO.builder().id(room.getId()).build();
     }
 
     @Override
     public RoomDetailedDTO rDetailedToDTO(Room room) {
-        return null;
+        return RoomDetailedDTO.builder().id(room.getId())
+                .roomNumber(room.getRoomNumber())
+                .price(room.getPrice())
+                .size(room.getSize())
+                .bookings(room.getBookings()
+                        .stream().map(b -> BookingDTO.builder()
+                                .id(b.getId()).build())
+                        .toList()).build();
     }
 
     @Override
-    public Room detailToRoom(RoomDetailedDTO roomDetailedDTO) {
-        return null;
+    public Room detailToRoom(RoomDetailedDTO room) {
+        return Room.builder().id(room.getId())
+                .roomNumber(room.getRoomNumber())
+                .price(room.getPrice())
+                .size(room.getSize())
+                .bookings(room.getBookings()
+                        .stream().map(b -> bookingRepo.findById(b.getId())
+                                .orElse(null))
+                        .toList()).build();
     }
 }
