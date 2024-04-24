2fd4ceafbfb429d0061065f35b7216c87d0d2bcc
Michael Speiner <speiner.m@gmail.com>
2024-04-24 10:33:01 +0200

    Created 2 of 3 service conversions

--- src/main/java/com/example/backendpensionat/Services/Impl/CustomerServiceIMPL.java
+++ src/main/java/com/example/backendpensionat/Services/Impl/CustomerServiceIMPL.java
@@ -1,19 +1,34 @@
 package com.example.backendpensionat.Services.Impl;
 
+import com.example.backendpensionat.DTO.BookingDTO;
 import com.example.backendpensionat.DTO.CustomerDTO;
 import com.example.backendpensionat.DTO.CustomerDetailedDTO;
 import com.example.backendpensionat.Models.Customer;
+import com.example.backendpensionat.Repos.BookingRepo;
+import com.example.backendpensionat.Repos.CustomerRepo;
 import com.example.backendpensionat.Services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+
+@RequiredArgsConstructor
+@Service
 public class CustomerServiceIMPL implements CustomerService {
+    BookingRepo bookingRepo;
+
     @Override
     public CustomerDTO customerToDTO(Customer customer) {
-        return null;
+        return CustomerDTO.builder().id(customer.getId()).build();
     }
 
     @Override
     public CustomerDetailedDTO cDetailedToDTO(Customer customer) {
-        return null;
+        return CustomerDetailedDTO.builder()
+                .id(customer.getId())
+                .firstName(customer
+                        .getFirstName())
+                .lastName(customer.getLastName()).email(customer.getEmail()).phone(customer.getPhone()).Ssn(customer.getSsn())
+                .bookings(customer.getBookings().stream().map(b -> BookingDTO.builder().id(b.getId()).build()).toList()).build();
     }
 
     @Override
