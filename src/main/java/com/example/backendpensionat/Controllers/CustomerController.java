529555838d6ae5142d8567b2b62094aa80cb64f2
Michael Speiner <speiner.m@gmail.com>
2024-04-26 10:37:27 +0200

    Created simple get controller for Customers

--- src/main/java/com/example/backendpensionat/Controllers/CustomerController.java
+++ src/main/java/com/example/backendpensionat/Controllers/CustomerController.java
@@ -1,7 +1,13 @@
 package com.example.backendpensionat.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {
+
+    @GetMapping("customers")
+    public String customers() {
+        return "customers";
+    }
 }
