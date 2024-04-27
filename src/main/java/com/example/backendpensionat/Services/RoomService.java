ee9673ac4cbb2ad97682fec41ff77694d296828c
Robin Blondin <robin.blondin@protonmail.com>
2024-04-28 00:04:44 +0200

    Declare new methods in service interface

--- src/main/java/com/example/backendpensionat/Services/RoomService.java
+++ src/main/java/com/example/backendpensionat/Services/RoomService.java
@@ -1,10 +1,18 @@
 package com.example.backendpensionat.Services;
 
+import com.example.backendpensionat.DTO.BookingDetailedDTO;
 import com.example.backendpensionat.DTO.RoomDTO;
 import com.example.backendpensionat.DTO.RoomDetailedDTO;
 import com.example.backendpensionat.Models.Room;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface RoomService {
+    List<RoomDetailedDTO> listAllRooms();
+    List<RoomDetailedDTO> listFreeRooms(LocalDate startDate, LocalDate endDate, int maxBeds);
+    LocalDate[] sortDates(LocalDate date1, LocalDate date2);
+    boolean isDateWithinBookingPeriod(LocalDate date, BookingDetailedDTO booking);
     RoomDTO roomToDTO(Room room);
     RoomDetailedDTO rDetailedToDTO(Room room);
     Room detailToRoom(RoomDetailedDTO roomDetailedDTO);
