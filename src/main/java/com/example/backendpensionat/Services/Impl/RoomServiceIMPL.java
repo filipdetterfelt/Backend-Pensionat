33680f9780f5db670aa1259425b6aad468d0c43d
Robin Blondin <robin.blondin@protonmail.com>
2024-04-28 00:06:32 +0200

    Implemented logic for newly declared methods

--- src/main/java/com/example/backendpensionat/Services/Impl/RoomServiceIMPL.java
+++ src/main/java/com/example/backendpensionat/Services/Impl/RoomServiceIMPL.java
@@ -1,19 +1,60 @@
 package com.example.backendpensionat.Services.Impl;
 
 import com.example.backendpensionat.DTO.BookingDTO;
+import com.example.backendpensionat.DTO.BookingDetailedDTO;
 import com.example.backendpensionat.DTO.RoomDTO;
 import com.example.backendpensionat.DTO.RoomDetailedDTO;
 import com.example.backendpensionat.Models.Room;
 import com.example.backendpensionat.Repos.BookingRepo;
+import com.example.backendpensionat.Repos.RoomRepo;
+import com.example.backendpensionat.Services.BookingService;
 import com.example.backendpensionat.Services.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class RoomServiceIMPL implements RoomService {
 
-    BookingRepo bookingRepo;
+    private final BookingRepo bookingRepo;
+    private final RoomRepo roomRepo;
+    private final BookingService bookingService;
+
+    @Override
+    public List<RoomDetailedDTO> listAllRooms() {
+        return roomRepo.findAll().stream().map(this::rDetailedToDTO).toList();
+    }
+
+    @Override
+    public List<RoomDetailedDTO> listFreeRooms(LocalDate startDate, LocalDate endDate, int maxBeds  ) {
+        LocalDate[] dates = sortDates(startDate, endDate);
+        return listAllRooms().stream().filter(room -> {
+            List<BookingDetailedDTO> bookings = room.getBookings().stream()
+                    .map(booking -> bookingService.bDetailedToDTO(bookingRepo.findById(booking.getId()).get()))
+                    .toList();
+
+                return bookings.stream().noneMatch(booking ->
+                       isDateWithinBookingPeriod(dates[0], booking) &&
+                       isDateWithinBookingPeriod(dates[1], booking) &&
+                       maxBeds <= room.getMaxBeds() + 1);
+        }).toList();
+    }
+
+    @Override
+    public LocalDate[] sortDates(LocalDate date1, LocalDate date2) {
+        if (date1.isBefore(date2)) return new LocalDate[]{date1, date2};
+        else if (date2.isBefore(date1)) return new LocalDate[]{date2, date1};
+        else return new LocalDate[]{date1, date2};
+    }
+
+    @Override
+    public boolean isDateWithinBookingPeriod(LocalDate date, BookingDetailedDTO booking) {
+        return (date.isEqual(booking.getStartDate()) || date.isAfter(booking.getStartDate())) &&
+               (date.isEqual(booking.getEndDate()) || date.isBefore(booking.getEndDate()));
+    }
 
     @Override
     public RoomDTO roomToDTO(Room room) {
@@ -25,6 +66,7 @@ public class RoomServiceIMPL implements RoomService {
         return RoomDetailedDTO.builder().id(room.getId())
                 .roomNumber(room.getRoomNumber())
                 .price(room.getPrice())
+                .maxBeds(room.getMaxBeds())
                 .size(room.getSize())
                 .bookings(room.getBookings()
                         .stream().map(b -> BookingDTO.builder()
@@ -37,6 +79,7 @@ public class RoomServiceIMPL implements RoomService {
         return Room.builder().id(room.getId())
                 .roomNumber(room.getRoomNumber())
                 .price(room.getPrice())
+                .maxBeds(room.getMaxBeds())
                 .size(room.getSize())
                 .bookings(room.getBookings()
                         .stream().map(b -> bookingRepo.findById(b.getId())
