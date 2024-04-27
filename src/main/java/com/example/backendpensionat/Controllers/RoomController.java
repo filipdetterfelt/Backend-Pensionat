8efc8f97bc6cf297eefc19d36133454b8a09eb95
Robin Blondin <robin.blondin@protonmail.com>
2024-04-28 00:07:57 +0200

    Added logic for room controllers

--- src/main/java/com/example/backendpensionat/Controllers/RoomController.java
+++ src/main/java/com/example/backendpensionat/Controllers/RoomController.java
@@ -1,13 +1,44 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.DTO.RoomDetailedDTO;
+import com.example.backendpensionat.Services.RoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class RoomController {
 
+    private final RoomService roomService;
+
     @GetMapping("/rooms")
-    public String allRooms() {
+    public String allRooms(Model model) {
+        if(!model.containsAttribute("roomsList")) {
+            model.addAttribute("roomsList", roomService.listAllRooms());
+        }
         return "rooms";
     }
+
+    @GetMapping("/rooms/search")
+    public String searchRooms(
+            @RequestParam(name = "startDate") LocalDate startDate,
+            @RequestParam(name = "endDate") LocalDate endDate,
+            @RequestParam(name = "amountOfPeople") int amountOfPeople,
+            RedirectAttributes model
+    ) {
+        startDate = startDate == null? LocalDate.now(): startDate;
+        endDate = endDate == null? LocalDate.now(): endDate;
+
+        List<RoomDetailedDTO> filteredRooms = roomService.listFreeRooms(startDate, endDate, amountOfPeople);
+        model.addFlashAttribute("roomsList", filteredRooms);
+        return "redirect:/rooms";
+    }
+
 }
