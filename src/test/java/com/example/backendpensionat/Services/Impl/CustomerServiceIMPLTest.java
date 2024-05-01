220ba2288967e1ea624324b0c105b0be0f722dba
Michael Speiner <speiner.m@gmail.com>
2024-05-01 09:13:16 +0200

    Created all instances needed to test customer

--- src/test/java/com/example/backendpensionat/Services/Impl/CustomerServiceIMPLTest.java
+++ src/test/java/com/example/backendpensionat/Services/Impl/CustomerServiceIMPLTest.java
@@ -1,5 +1,10 @@
 package com.example.backendpensionat.Services.Impl;
 
+import com.example.backendpensionat.DTO.CustomerDTO;
+import com.example.backendpensionat.DTO.CustomerDetailedDTO;
+import com.example.backendpensionat.Models.Booking;
+import com.example.backendpensionat.Models.Customer;
+import com.example.backendpensionat.Models.Room;
 import com.example.backendpensionat.Repos.BookingRepo;
 import com.example.backendpensionat.Repos.CustomerRepo;
 import org.junit.jupiter.api.Test;
@@ -8,17 +13,47 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CustomerServiceIMPLTest {
 
+    Long id = 1L;
+    String firstName = "Micke";
+    String lastName = "Speiner";
+    String email = "micke@speiner.com";
+    String phone = "0701515151";
+    String Ssn = "970217-9797";
+    List<Booking> BookingList = new ArrayList<>();
+
+
+    //Creating customer, room, booking
+    Customer customer = new Customer(id, firstName, lastName, email, phone, Ssn, BookingList);
+    Room room = new Room(1L, 101L, 150.0, 2, 20, BookingList);
+    Booking booking = new Booking(1L, 2, 100.0, LocalDate.now(), LocalDate.now().plusDays(3), customer, room);
+
+    //Creating undeatailedCustomerDTO
+    CustomerDTO smallDTO = CustomerDTO.builder().id(customer.getId()).build();
+
+    //Creating detailedCustomerDTO
+    Customer detailedCustomerDTO = Customer.builder()
+            .id(id)
+            .firstName(firstName)
+            .lastName(lastName)
+            .email(email)
+            .phone(phone)
+            .Ssn(Ssn)
+            .bookings(new ArrayList<>())
+            .build();
     @Mock
     private CustomerRepo customerRepo;
-
     @Mock
     private BookingRepo bookingRepo;
-
     @InjectMocks
     private CustomerServiceIMPL customerServiceIMPL = new CustomerServiceIMPL(bookingRepo, customerRepo);
 
@@ -44,6 +79,7 @@ class CustomerServiceIMPLTest {
 
     @Test
     void cDetailedToDTO() {
+        CustomerDetailedDTO actual = customerServiceIMPL.cDetailedToDTO(customer);
     }
 
     @Test
