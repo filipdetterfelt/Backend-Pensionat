a00a92ba206f8867244ca9a1b5e9abc574c6a283
Michael Speiner <speiner.m@gmail.com>
2024-05-01 16:18:58 +0200

    Fixed broken tests after merge

--- src/main/java/com/example/backendpensionat/DTO/BookingDetailedDTO.java
+++ src/main/java/com/example/backendpensionat/DTO/BookingDetailedDTO.java
@@ -19,7 +19,7 @@ public class BookingDetailedDTO {
     private Double totalPrice;
     private LocalDate startDate;
     private LocalDate endDate;
-    private final String roomNumber = "";
+    private String roomNumber = "";
 
     private RoomDetailedDTO room;
     private CustomerDTO customerDTO;
