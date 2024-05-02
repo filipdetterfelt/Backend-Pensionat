29610c84a1df163e6fb82ce2fdedb09737d8b0d8
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-05-02 15:49:33 +0200

    AddBookingsForm Fixes

--- src/main/java/com/example/backendpensionat/Models/Room.java
+++ src/main/java/com/example/backendpensionat/Models/Room.java
@@ -21,7 +21,7 @@ public class Room {
     private Long id;
     private Long roomNumber;
 
-   // @Pattern(regexp = "[0-9.]+", message = "Price can only include digits and dots")
+    //@Pattern(regexp = "[0-9.]+", message = "Price can only include digits and dots")
     private Double price;
     //@Pattern(regexp = "[0-9]+", message = "size can only include digits")
     private RoomType roomType;
