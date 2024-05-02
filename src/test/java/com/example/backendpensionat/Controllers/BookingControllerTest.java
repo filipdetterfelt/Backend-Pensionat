3f713b423a4a33ba1f026088ed54991b89268ca1
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-05-02 10:02:51 +0200

    RoomController Tests

--- src/test/java/com/example/backendpensionat/Controllers/BookingControllerTest.java
+++ src/test/java/com/example/backendpensionat/Controllers/BookingControllerTest.java
@@ -1,10 +1,24 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.Enums.RoomType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureMockMvc
@@ -16,6 +30,10 @@ class BookingControllerTest {
 
     @Test
     void TestAllBookings() throws Exception {
+        this.mockMvc.perform(get("/bookings"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("bookings")));
     }
 
     @Test
@@ -24,6 +42,10 @@ class BookingControllerTest {
 
     @Test
     void TestAddBookings() throws Exception {
+        this.mockMvc.perform(get("/bookings/addBookings"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<option value=\"3\">3: Karin Nilsson</option>")));
     }
 
     @Test
@@ -31,7 +53,15 @@ class BookingControllerTest {
     }
 
     @Test
-    void TestEditRefresh() {
+    void TestEditRefresh() throws Exception {
+        LocalDate startDate = LocalDate.of(2024, 5, 1);
+        LocalDate endDate = LocalDate.of(2024, 5, 6);
+        Long bookingId = 1L;
+
+        this.mockMvc.perform(get("/bookings/edit/refresh/{startDate}/{endDate}/{bookingId}", startDate, endDate, bookingId))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/bookings/" + bookingId + "/edit"));
     }
 
     @Test
@@ -39,7 +69,17 @@ class BookingControllerTest {
     }
 
     @Test
-    void TestSearchRoom() {
+    void TestSearchRoom() throws Exception {
+        Long id = 1L;
+        LocalDate startDate = LocalDate.of(2024, 5, 1);
+        LocalDate endDate = LocalDate.of(2024, 5, 6);
+        int roomType = 0;
+
+
+        this.mockMvc.perform(get("/bookings/add/{id}/{startDate}/{endDate}/{roomType}", id, startDate, endDate, roomType))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/bookings/addBookings"));
     }
 
     @Test
