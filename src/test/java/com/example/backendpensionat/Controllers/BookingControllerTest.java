85465ac205505e7b1842c3ac2bdd7897962642bd
Michael Speiner <speiner.m@gmail.com>
2024-05-02 09:19:47 +0200

    Booking controller

---
+++ src/test/java/com/example/backendpensionat/Controllers/BookingControllerTest.java
@@ -0,0 +1,48 @@
+package com.example.backendpensionat.Controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+@AutoConfigureMockMvc
+@SpringBootTest
+class BookingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void TestAllBookings() throws Exception {
+    }
+
+    @Test
+    void TestDeleteBookingById() throws Exception {
+    }
+
+    @Test
+    void TestAddBookings() throws Exception {
+    }
+
+    @Test
+    void TestEditBookingById() {
+    }
+
+    @Test
+    void TestEditRefresh() {
+    }
+
+    @Test
+    void TestUpdateBookingPost() {
+    }
+
+    @Test
+    void TestSearchRoom() {
+    }
+
+    @Test
+    void TestAddBooking() {
+    }
+}
