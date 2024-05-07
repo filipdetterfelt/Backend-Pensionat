b013ca1b1e19e6f5af9dc1c6fe78ffec2a32781d
Michael Speiner <speiner.m@gmail.com>
2024-05-07 13:21:43 +0200

    Wrote builders for ContractCustomer and ContractCustomerDTO

--- src/main/java/com/example/backendpensionat/Services/Impl/ContractCustomerServiceIMPL.java
+++ src/main/java/com/example/backendpensionat/Services/Impl/ContractCustomerServiceIMPL.java
@@ -1,6 +1,37 @@
 package com.example.backendpensionat.Services.Impl;
 
+import com.example.backendpensionat.DTO.ContractCustomerDTO;
+import com.example.backendpensionat.Models.ContractCustomer;
 import com.example.backendpensionat.Services.ContractCustomerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ContractCustomerServiceIMPL implements ContractCustomerService {
+
+    public ContractCustomerDTO CmsToDTO(ContractCustomer ContractCustomer){
+        return ContractCustomerDTO.builder()
+                .externalId(ContractCustomer.externalId)
+                .companyName(ContractCustomer.companyName)
+                .contactName(ContractCustomer.contactName)
+                .contactTitle(ContractCustomer.contactTitle)
+                .streetAddress(ContractCustomer.streetAddress)
+                .city(ContractCustomer.city)
+                .postalCode(ContractCustomer.postalCode)
+                .country(ContractCustomer.country)
+                .phone(ContractCustomer.phone)
+                .fax(ContractCustomer.fax).build();
+    }
+
+    public ContractCustomer detailToCms(ContractCustomerDTO ContractCustomerDTO){
+        return ContractCustomer.builder()
+                .externalId(ContractCustomerDTO.externalId)
+                .companyName(ContractCustomerDTO.companyName)
+                .contactName(ContractCustomerDTO.contactName)
+                .streetAddress(ContractCustomerDTO.streetAddress)
+                .city(ContractCustomerDTO.city)
+                .postalCode(ContractCustomerDTO.postalCode)
+                .country(ContractCustomerDTO.country)
+                .phone(ContractCustomerDTO.phone)
+                .fax(ContractCustomerDTO.fax).build();
+    }
 }
