7af3fbb61d2dc6e3b5ae91271695c2789892665f
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-05-07 09:42:30 +0200

    Variables and lists added

--- src/main/java/com/example/backendpensionat/DTO/ContractCustomerDTO.java
+++ src/main/java/com/example/backendpensionat/DTO/ContractCustomerDTO.java
@@ -1,4 +1,14 @@
 package com.example.backendpensionat.DTO;
 
 public class ContractCustomerDTO {
+    public int id;
+    public String companyName;
+    public String contactName;
+    public String contactTitle;
+    public String streetAddress;
+    public String city;
+    public int postalCode;
+    public String country;
+    public String phone;
+    public String fax;
 }
