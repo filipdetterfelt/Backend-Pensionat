9e7a09fc3eb10c6eaf18ff0cf97d5b3ae106130c
Michael Speiner <speiner.m@gmail.com>
2024-05-07 14:37:56 +0200

    Fixed ShippersRepo

--- src/main/java/com/example/backendpensionat/Repos/ShippersRepo.java
+++ src/main/java/com/example/backendpensionat/Repos/ShippersRepo.java
@@ -3,5 +3,5 @@ package com.example.backendpensionat.Repos;
 import com.example.backendpensionat.Models.Shippers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShippersRepo extends JpaRepository<Long, Shippers> {
+public interface ShippersRepo extends JpaRepository<Shippers, Long> {
 }
