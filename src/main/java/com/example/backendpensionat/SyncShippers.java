ee2743c61582f5d0444600b67340c75faf84e624
Robin Blondin <robin.blondin@protonmail.com>
2024-05-07 09:17:29 +0200

    Created CommandLineRunner class for syncing shippers

---
+++ src/main/java/com/example/backendpensionat/SyncShippers.java
@@ -0,0 +1,10 @@
+package com.example.backendpensionat;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class SyncShippers implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
