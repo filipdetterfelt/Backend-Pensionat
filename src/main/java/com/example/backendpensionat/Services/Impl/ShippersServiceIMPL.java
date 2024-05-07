b9d2187793962ee99ca1353fc82f654b317ddd81
Filip <fillip.detterfelt@gmail.com>
2024-05-07 13:17:47 +0200

    Started ShippersServiceIMPL

--- src/main/java/com/example/backendpensionat/Services/Impl/ShippersServiceIMPL.java
+++ src/main/java/com/example/backendpensionat/Services/Impl/ShippersServiceIMPL.java
@@ -1,8 +1,19 @@
 package com.example.backendpensionat.Services.Impl;
 
+import com.example.backendpensionat.DTO.BookingDetailedDTO;
+import com.example.backendpensionat.DTO.ShippersDTO;
+import com.example.backendpensionat.DTO.ShippersDetailedDTO;
+import com.example.backendpensionat.Models.Booking;
+import com.example.backendpensionat.Models.Shippers;
 import com.example.backendpensionat.Services.ShippersService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShippersServiceIMPL implements ShippersService {
+
+
+    @Override
+    public ShippersDTO shippersToDTO(Shippers shippers) {
+        return null;
+    }
 }
--- src/main/java/com/example/backendpensionat/Services/ShippersService.java
+++ src/main/java/com/example/backendpensionat/Services/ShippersService.java
@@ -1,4 +1,13 @@
 package com.example.backendpensionat.Services;
 
+import com.example.backendpensionat.DTO.BookingDTO;
+import com.example.backendpensionat.DTO.BookingDetailedDTO;
+import com.example.backendpensionat.DTO.ShippersDTO;
+import com.example.backendpensionat.DTO.ShippersDetailedDTO;
+import com.example.backendpensionat.Models.Booking;
+import com.example.backendpensionat.Models.Shippers;
+
 public interface ShippersService {
+    ShippersDTO shippersToDTO(Shippers shippers);
+
 }
