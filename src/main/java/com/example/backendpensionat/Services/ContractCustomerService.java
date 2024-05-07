ba345e90c217c7f4e71c690654f3343b3dcbf498
Michael Speiner <speiner.m@gmail.com>
2024-05-07 13:19:03 +0200

    Created interface for ContractCustomerService 2 methods

--- src/main/java/com/example/backendpensionat/Services/ContractCustomerService.java
+++ src/main/java/com/example/backendpensionat/Services/ContractCustomerService.java
@@ -1,4 +1,12 @@
 package com.example.backendpensionat.Services;
 
+import com.example.backendpensionat.DTO.ContractCustomerDTO;
+import com.example.backendpensionat.DTO.CustomerDTO;
+import com.example.backendpensionat.DTO.CustomerDetailedDTO;
+import com.example.backendpensionat.Models.ContractCustomer;
+import com.example.backendpensionat.Models.Customer;
+
 public interface ContractCustomerService {
+    ContractCustomerDTO CmsToDTO(ContractCustomer ContractCustomer);
+    ContractCustomer detailToCms(ContractCustomerDTO ContractCustomerDTO);
 }
