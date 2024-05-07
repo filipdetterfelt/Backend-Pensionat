0294aea4efe0781d59b74cfacfa142c940c02086
Filip <fillip.detterfelt@gmail.com>
2024-05-07 09:28:09 +0200

    Edit files for shippers

--- src/main/java/com/example/backendpensionat/Controllers/ShippersController.java
+++ src/main/java/com/example/backendpensionat/Controllers/ShippersController.java
@@ -1,4 +1,21 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.Services.ShippersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class ShippersController {
+
+    private final ShippersService service;
+
+    @GetMapping("/shippers")
+    public String allShippers(Model model){
+     return "EJ KLAR!!!!";
+    }
+
+
 }
--- src/main/java/com/example/backendpensionat/DTO/ShippersDTO.java
+++ src/main/java/com/example/backendpensionat/DTO/ShippersDTO.java
@@ -1,4 +1,17 @@
 package com.example.backendpensionat.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class ShippersDTO {
+    private Long Id;
+
+    private String CompanyName;
+    private String Phone;
 }
