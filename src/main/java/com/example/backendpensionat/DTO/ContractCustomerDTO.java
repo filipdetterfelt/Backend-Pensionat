888358a9bfe0fff97ff2b3330760bbc6723ba263
Robin Blondin <robin.blondin@protonmail.com>
2024-05-07 11:36:02 +0200

    Added annotations for mapping tables and column names

--- src/main/java/com/example/backendpensionat/DTO/ContractCustomerDTO.java
+++ src/main/java/com/example/backendpensionat/DTO/ContractCustomerDTO.java
@@ -1,7 +1,21 @@
 package com.example.backendpensionat.DTO;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "customers")
 public class ContractCustomerDTO {
-    public Long id;
+    @JacksonXmlProperty(localName = "id")
+    public Long externalId;
+
     public String companyName;
     public String contactName;
     public String contactTitle;
