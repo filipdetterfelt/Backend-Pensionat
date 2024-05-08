a70d4fffc5e06a02aabe1627a2347a84815fea3c
Filip <fillip.detterfelt@gmail.com>
2024-05-08 12:36:16 +0200

    Added method

--- src/main/java/com/example/backendpensionat/Services/ShippersService.java
+++ src/main/java/com/example/backendpensionat/Services/ShippersService.java
@@ -1,4 +1,8 @@
 package com.example.backendpensionat.Services;
 
+import com.example.backendpensionat.DTO.ShippersDetailedDTO;
+import com.example.backendpensionat.DTO.ShippersTest;
+
 public interface ShippersService {
+    ShippersTest saveShipper(ShippersDetailedDTO dto);
 }
