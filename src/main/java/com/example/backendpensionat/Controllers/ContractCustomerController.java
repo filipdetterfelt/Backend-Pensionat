76303e974649bdcb61670b222ddfad541694e285
Filip <fillip.detterfelt@gmail.com>
2024-05-08 16:34:04 +0200

    Build Controller for list all contract customers

--- src/main/java/com/example/backendpensionat/Controllers/ContractCustomerController.java
+++ src/main/java/com/example/backendpensionat/Controllers/ContractCustomerController.java
@@ -1,4 +1,21 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.Models.ContractCustomer;
+import com.example.backendpensionat.Services.ContractCustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class ContractCustomerController {
+
+    private final ContractCustomerService contractCustomerService;
+
+    @GetMapping("/contractCustomers")
+    public String contractCustomers(Model model){
+        model.addAttribute("contractCustomersList",contractCustomerService.listAllContractCustomers());
+        return "contractCustomers";
+    }
 }
