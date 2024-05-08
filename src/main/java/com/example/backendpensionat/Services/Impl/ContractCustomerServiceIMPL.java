7e4ecebcef003984ff890727ea68b643ec8d1cce
Robin Blondin <robin.blondin@protonmail.com>
2024-05-08 23:29:36 +0200

    Created search Query for filtering and sorting contract customers

--- src/main/java/com/example/backendpensionat/Services/Impl/ContractCustomerServiceIMPL.java
+++ src/main/java/com/example/backendpensionat/Services/Impl/ContractCustomerServiceIMPL.java
@@ -1,14 +1,17 @@
 package com.example.backendpensionat.Services.Impl;
 
 import com.example.backendpensionat.DTO.ContractCustomerDTO;
+import com.example.backendpensionat.DTO.RoomDetailedDTO;
 import com.example.backendpensionat.Models.ContractCustomer;
+import com.example.backendpensionat.Models.Room;
 import com.example.backendpensionat.Repos.ContractCustomerRepo;
 import com.example.backendpensionat.Services.ContractCustomerService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ContractCustomerServiceIMPL implements ContractCustomerService {
@@ -16,9 +19,45 @@ public class ContractCustomerServiceIMPL implements ContractCustomerService {
     @Autowired
     ContractCustomerRepo contractCustomerRepo;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
-    public ContractCustomer saveContractCustomer(ContractCustomerDTO cCustomer) {
-        return contractCustomerRepo.save(detailToCms(cCustomer));
+    public void saveContractCustomer(ContractCustomerDTO cCustomer) {
+        contractCustomerRepo.save(detailToCms(cCustomer));
+    }
+
+    @Override
+    public List<ContractCustomerDTO> listSortedContractCustomers(String searchWord, String columnName, String sortingOrder) {
+        String jpqlQuery = String.format(
+                "SELECT c FROM ContractCustomer c WHERE " +
+                        "c.companyName LIKE :searchWord OR " +
+                        "c.contactName LIKE :searchWord OR " +
+                        "c.country LIKE :searchWord OR " +
+                        "ORDER BY %s %s", columnName, sortingOrder);
+
+        return entityManager.createQuery(jpqlQuery, ContractCustomer.class)
+                .setParameter("searchWord", "%" + searchWord + "%")
+                .getResultList().stream()
+                .map(customer -> ContractCustomerDTO.builder()
+                        .internalId(customer.getId())
+                        .externalId(customer.getExternalId())
+                        .companyName(customer.getCompanyName())
+                        .contactName(customer.getContactName())
+                        .contactTitle(customer.getContactTitle())
+                        .streetAddress(customer.getStreetAddress())
+                        .city(customer.getCity())
+                        .postalCode(customer.getPostalCode())
+                        .country(customer.getCountry())
+                        .phone(customer.getPhone())
+                        .fax(customer.getFax())
+                        .build())
+                .toList();
+    }
+
+    @Override
+    public List<ContractCustomerDTO> allCustomers() {
+        return contractCustomerRepo.findAll().stream().map(this::CmsToDTO).toList();
     }
 
     public ContractCustomerDTO CmsToDTO(ContractCustomer ContractCustomer) {
