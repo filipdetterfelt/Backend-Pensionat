494d31f21e4b03b66dedfb07b841a75ebc3807b8
Filip <fillip.detterfelt@gmail.com>
2024-05-08 12:36:04 +0200

    Not finished method for saveShipper

--- src/main/java/com/example/backendpensionat/Services/Impl/ShippersServiceIMPL.java
+++ src/main/java/com/example/backendpensionat/Services/Impl/ShippersServiceIMPL.java
@@ -1,8 +1,20 @@
 package com.example.backendpensionat.Services.Impl;
 
+import com.example.backendpensionat.DTO.ShippersDetailedDTO;
+import com.example.backendpensionat.DTO.ShippersTest;
+import com.example.backendpensionat.Repos.ShippersRepo;
 import com.example.backendpensionat.Services.ShippersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShippersServiceIMPL implements ShippersService {
+
+    @Autowired
+    ShippersRepo shippersRepo;
+
+    @Override
+    public ShippersTest saveShipper(ShippersDetailedDTO dto){
+        return shippersRepo.save(detailToCms(dto));
+    }
 }
