309091e1829116bb0bc359de69836b66cd36e3db
Robin Blondin <robin.blondin@protonmail.com>
2024-05-08 22:07:42 +0200

    Added method for checking blacklist

--- src/main/java/com/example/backendpensionat/Services/BlacklistService.java
+++ src/main/java/com/example/backendpensionat/Services/BlacklistService.java
@@ -1,4 +1,7 @@
 package com.example.backendpensionat.Services;
 
+import com.example.backendpensionat.DTO.BlacklistDetailedDTO;
+
 public interface BlacklistService {
+    BlacklistDetailedDTO checkBlackList(String email);
 }
