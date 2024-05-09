118afa6325f090e590361202da3a4fe122c82716
Robin Blondin <robin.blondin@protonmail.com>
2024-05-09 17:40:11 +0200

    Added controller methods for sorting and searching

--- src/main/java/com/example/backendpensionat/Controllers/ContractCustomerController.java
+++ src/main/java/com/example/backendpensionat/Controllers/ContractCustomerController.java
@@ -1,17 +1,15 @@
 package com.example.backendpensionat.Controllers;
 
 import com.example.backendpensionat.DTO.ContractCustomerDTO;
-import com.example.backendpensionat.DTO.ContractCustomerDetailedDTO;
-import com.example.backendpensionat.Models.ContractCustomer;
+import com.example.backendpensionat.DTO.ContractCustomersSearchDTO;
 import com.example.backendpensionat.Services.ContractCustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,8 +19,43 @@ public class ContractCustomerController {
 
     @GetMapping("/ContractCustomers")
     public String contractCustomers(Model model){
-        model.addAttribute("contractCustomersList",contractCustomerService.listAllContractCustomers());
+        if(!model.containsAttribute("contractCustomersList")) {
+            model.addAttribute("contractCustomersList", contractCustomerService.listAllContractCustomers());
+        }
         return "contractCustomers";
+    }
+
+    @GetMapping("/ContractCustomers/search")
+    public String searchCustomer(
+            @ModelAttribute ContractCustomersSearchDTO contractCustomersSearchDTO,
+            RedirectAttributes rda) {
+
+        String column = contractCustomersSearchDTO.orderString.split(" - ")[0];
+        String order = contractCustomersSearchDTO.orderString.split(" - ")[1];
+
+        List<ContractCustomerDTO> list = contractCustomerService.listSortedContractCustomers(column, order);
+
+        List<ContractCustomerDTO> filteredList = list.stream().filter(customer ->
+                customer.getCompanyName().contains(contractCustomersSearchDTO.searchWord) ||
+                customer.getContactName().contains(contractCustomersSearchDTO.searchWord) ||
+                customer.getCountry().contains(contractCustomersSearchDTO.searchWord)).toList();
+
+        rda.addFlashAttribute("contractCustomersList", filteredList);
+        rda.addFlashAttribute("sortString", contractCustomersSearchDTO.orderString);
+        return "redirect:/ContractCustomers";
+    }
+
+    @GetMapping("ContractCustomers/sort/{sortString}")
+    public String sortCustomers(@PathVariable String sortString,
+                                RedirectAttributes rda) {
+        String column = sortString.split(" - ")[0];
+        String order = sortString.split(" - ")[1];
+
+        List<ContractCustomerDTO> sortedCustomers = contractCustomerService.listSortedContractCustomers(column, order);
+
+        rda.addFlashAttribute("contractCustomersList", sortedCustomers);
+        rda.addFlashAttribute("sortString", sortString);
+        return "redirect:/ContractCustomers";
     }
 
     /*@PostMapping("deleteContractCustomer")
