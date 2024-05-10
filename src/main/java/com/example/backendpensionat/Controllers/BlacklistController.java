50f455c6e17dca16a7fb74b56f0f8ee1bfcfcf90
Michael Speiner <speiner.m@gmail.com>
2024-05-10 11:23:14 +0200

    Added respons to blacklist

--- src/main/java/com/example/backendpensionat/Controllers/BlacklistController.java
+++ src/main/java/com/example/backendpensionat/Controllers/BlacklistController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,26 +15,28 @@ public class BlacklistController {
     private final BlacklistServiceIMPL blacklistService;
 
     @GetMapping("/blacklistCustomers")
-    public String showBlacklistForm(Model model) {
-        model.addAttribute("blacklistCustomers", new BlacklistDetailedDTO());
+    public String showBlacklistForm() {
         return "blacklistCustomers";
     }
 
     @PostMapping("/blacklist/add")
-    public String addCustomerToBlacklist(@RequestParam(name = "email") String email, @RequestParam(name = "name")  String name) {
+    public String addCustomerToBlacklist(@RequestParam(name = "email") String email, @RequestParam(name = "name")  String name, RedirectAttributes rda) {
         blacklistService.addCustomerToBlacklist(email, name);
+        rda.addFlashAttribute("isAdded", true);
         return "redirect:/blacklistCustomers";
     }
 
     @GetMapping("/blacklist/updateTrue")
-    public String updateBlacklistStatusTrue(@RequestParam(name = "email") String email) {
+    public String updateBlacklistStatusTrue(@RequestParam(name = "email") String email, RedirectAttributes rda) {
         blacklistService.updateCustomerInBlacklistToTrue(email);
+        rda.addFlashAttribute("isGreenlisted", true);
         return "redirect:/blacklistCustomers";
     }
 
     @GetMapping("/blacklist/updateFalse")
-    public String updateBlacklistStatusFalse(@RequestParam(name = "email") String email) {
+    public String updateBlacklistStatusFalse(@RequestParam(name = "email") String email, RedirectAttributes rda) {
         blacklistService.updateCustomerInBlacklistToFalse(email);
+        rda.addFlashAttribute("isBlacklisted", true);
         return "redirect:/blacklistCustomers";
     }
 }
