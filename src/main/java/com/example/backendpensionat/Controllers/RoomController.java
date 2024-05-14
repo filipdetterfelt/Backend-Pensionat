07b64bb9872d785eaeabe63011f50008eae54a38
Filip <fillip.detterfelt@gmail.com>
2024-05-14 14:48:17 +0200

    Added controller for roomLog

--- src/main/java/com/example/backendpensionat/Controllers/RoomController.java
+++ src/main/java/com/example/backendpensionat/Controllers/RoomController.java
@@ -2,12 +2,16 @@ package com.example.backendpensionat.Controllers;
 
 import com.example.backendpensionat.DTO.RoomDetailedDTO;
 import com.example.backendpensionat.DTO.RoomSearchDTO;
+import com.example.backendpensionat.Models.ContractCustomer;
+import com.example.backendpensionat.Models.Room;
+import com.example.backendpensionat.Models.RoomEventHappenings.EventType;
 import com.example.backendpensionat.Services.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
@@ -30,5 +34,16 @@ public class RoomController {
         model.addAttribute("roomsList", filteredRooms);
         return "rooms";
     }
+
+
+
+   @GetMapping("/roomLog/{id}")
+   public String showLogs(@PathVariable Long id, Model model){
+        model.addAttribute("roomLogList", roomService.findRoomById(id));
+        model.addAttribute("roomSearch", new RoomSearchDTO());
+        return "roomLog";
+   }
+
+
 
 }
