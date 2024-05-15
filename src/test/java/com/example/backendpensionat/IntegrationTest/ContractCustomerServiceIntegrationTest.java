fef26a7b83b5eea15bbb26b66652eb05f9e50c09
Robin Blondin <robin.blondin@protonmail.com>
2024-05-15 23:20:17 +0200

    Renamed files and added integration tests of service classes

---
+++ src/test/java/com/example/backendpensionat/IntegrationTest/ContractCustomerServiceIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.example.backendpensionat.IntegrationTest;
+
+import com.example.backendpensionat.DTO.ContractCustomerDTO;
+import com.example.backendpensionat.Repos.ContractCustomerRepo;
+import com.example.backendpensionat.Services.ContractCustomerService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ContractCustomerServiceIntegrationTest {
+
+    @Autowired
+    ContractCustomerRepo contractCustomerRepo;
+
+    @Autowired
+    ContractCustomerService sut;
+
+
+
+    @Test
+    void getContractCustomersFromXMLTest() throws Exception {
+        URL url = new URL("https://javaintegration.systementor.se/customers");
+        assertDoesNotThrow(() -> sut.getContractCustomersFromXML(url));
+
+        List<ContractCustomerDTO> contractCustomers = sut.getContractCustomersFromXML(url);
+        assertFalse(contractCustomers.isEmpty());
+        assertNotNull(contractCustomers.get(0).externalId);
+        assertNotNull(contractCustomers.get(0).companyName);
+        assertNotNull(contractCustomers.get(0).contactName);
+        assertNotNull(contractCustomers.get(0).contactTitle);
+        assertNotNull(contractCustomers.get(0).streetAddress);
+        assertNotNull(contractCustomers.get(0).postalCode);
+        assertNotNull(contractCustomers.get(0).phone);
+        assertNotNull(contractCustomers.get(0).country);
+        assertNotNull(contractCustomers.get(0).fax);
+        assertNotNull(contractCustomers.get(0).city);
+        assertNull(contractCustomers.get(0).getInternalId());
+    }
+
+    @Test
+    void getAndSaveContractCustomers() throws IOException {
+        contractCustomerRepo.deleteAll();
+        sut.getAndSaveContractCustomers(true);
+
+        assertEquals(3, contractCustomerRepo.count());
+    }
+
+}
---
+++ src/test/java/com/example/backendpensionat/IntegrationTest/ShippersServiceIntegrationTest.java
@@ -0,0 +1,52 @@
+package com.example.backendpensionat.IntegrationTest;
+
+import com.example.backendpensionat.DTO.ShippersDetailedDTO;
+import com.example.backendpensionat.Repos.ShippersRepo;
+import com.example.backendpensionat.Services.ShippersService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ShippersServiceIntegrationTest {
+
+    @Autowired
+    ShippersRepo shippersRepo;
+
+    @Autowired
+    ShippersService sut;
+
+    @Test
+    void getShippersFromJson() throws Exception {
+        URL url = new URL("https://javaintegration.systementor.se/shippers");
+        assertDoesNotThrow(() -> sut.getShippersFromJSON(url));
+
+        List<ShippersDetailedDTO> shipper = sut.getShippersFromJSON(url);
+        assertFalse(shipper.isEmpty());
+        assertNull(shipper.get(0).internalId);
+        assertNotNull(shipper.get(0).externalId);
+        assertNotNull(shipper.get(0).email);
+        assertNotNull(shipper.get(0).companyName);
+        assertNotNull(shipper.get(0).contactName);
+        assertNotNull(shipper.get(0).contactTitle);
+        assertNotNull(shipper.get(0).streetAddress);
+        assertNotNull(shipper.get(0).city);
+        assertNotNull(shipper.get(0).postalCode);
+        assertNotNull(shipper.get(0).country);
+        assertNotNull(shipper.get(0).phone);
+        assertNotNull(shipper.get(0).fax);
+    }
+
+    @Test
+    void getAndSaveContractCustomers() throws IOException {
+        shippersRepo.deleteAll();
+        sut.getAndSaveShippers(true);
+
+        assertEquals(3, shippersRepo.count());
+    }
+
+}
