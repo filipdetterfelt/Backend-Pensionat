5e4249621640ace74cd823bde2c62ab9cdd8de31
Robin Blondin <robin.blondin@protonmail.com>
2024-05-15 23:18:58 +0200

    Added query method

--- src/main/java/com/example/backendpensionat/Repos/ContractCustomerRepo.java
+++ src/main/java/com/example/backendpensionat/Repos/ContractCustomerRepo.java
@@ -3,5 +3,8 @@ package com.example.backendpensionat.Repos;
 import com.example.backendpensionat.Models.ContractCustomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContractCustomerRepo extends JpaRepository<ContractCustomer,Long>{
+    Optional<ContractCustomer> findContractCustomerByExternalId(Long id);
 }
--- src/main/java/com/example/backendpensionat/Repos/ShippersRepo.java
+++ src/main/java/com/example/backendpensionat/Repos/ShippersRepo.java
@@ -3,5 +3,8 @@ package com.example.backendpensionat.Repos;
 import com.example.backendpensionat.Models.Shippers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShippersRepo extends JpaRepository<Shippers, Long> {
+    Optional<Shippers> findShippersByExternalId(Long id);
 }
