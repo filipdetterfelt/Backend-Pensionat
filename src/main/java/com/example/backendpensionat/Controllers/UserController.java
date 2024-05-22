5a15cd8c618cef14e23c6d88fc79ef512862c4b4
Filip <fillip.detterfelt@gmail.com>
2024-05-22 15:01:20 +0200

    Added controller

--- src/main/java/com/example/backendpensionat/Controllers/UserController.java
+++ src/main/java/com/example/backendpensionat/Controllers/UserController.java
@@ -1,10 +1,20 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.Services.Impl.UserDetailsServiceIMPL;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
+
+    public UserDetailsServiceIMPL userDetailsService;
+
+    @GetMapping("/Users")
+    public String Users(){
+        return "users";
+    }
 
 }
