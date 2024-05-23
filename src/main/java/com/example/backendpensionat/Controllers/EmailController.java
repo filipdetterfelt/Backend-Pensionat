4a568bb9fd907d75a18796515f43dbdc0b4a37b3
Michael Speiner <speiner.m@gmail.com>
2024-05-23 15:00:18 +0200

    Set up HTML file for email conf

--- src/main/java/com/example/backendpensionat/Controllers/EmailController.java
+++ src/main/java/com/example/backendpensionat/Controllers/EmailController.java
@@ -1,21 +1,20 @@
 package com.example.backendpensionat.Controllers;
 
 import com.example.backendpensionat.Services.Impl.EmailServiceIMPL;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@AllArgsConstructor
+@Controller
 public class EmailController {
 
     private final EmailServiceIMPL emailServiceIMPL;
 
-    public EmailController(EmailServiceIMPL emailServiceIMPL) {
-        this.emailServiceIMPL = emailServiceIMPL;
-    }
-
     @RequestMapping("/sendConfirmationEmail")
     public String sendConfirmationEmail() {
         emailServiceIMPL.sendEmail("michael.s_97@hotmail.com", "Test", "Test");
-        return "Email sent";
+        return "emailConfirmation";
     }
 }
