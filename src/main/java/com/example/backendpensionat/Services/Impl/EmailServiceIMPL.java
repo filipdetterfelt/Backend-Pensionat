92dfcecafae23cef9a16708ac4d6131597ceb2c6
Michael Speiner <speiner.m@gmail.com>
2024-05-23 13:20:46 +0200

    Created Service class for the confirmation email function

---
+++ src/main/java/com/example/backendpensionat/Services/Impl/EmailServiceIMPL.java
@@ -0,0 +1,26 @@
+package com.example.backendpensionat.Services.Impl;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceIMPL {
+
+    private JavaMailSender mailSender;
+
+    public EmailServiceIMPL(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("speiner.m@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
+}
