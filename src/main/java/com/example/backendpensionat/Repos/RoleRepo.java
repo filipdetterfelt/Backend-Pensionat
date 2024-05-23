01f4f6c922ea996a69c2436ee3774d221867d2bc
Robin Blondin <robin.blondin@protonmail.com>
2024-05-23 12:28:06 +0200

    fixed error with extending wrong class

--- src/main/java/com/example/backendpensionat/Repos/RoleRepo.java
+++ src/main/java/com/example/backendpensionat/Repos/RoleRepo.java
@@ -1,11 +1,11 @@
 package com.example.backendpensionat.Repos;
 
 import com.example.backendpensionat.Models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface RoleRepo extends CrudRepository<Role, UUID> {
-
+public interface RoleRepo extends JpaRepository<Role, UUID> {
     Role findByName(String name);
 }
