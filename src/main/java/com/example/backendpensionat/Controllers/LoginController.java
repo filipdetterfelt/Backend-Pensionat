584138e2cd85d001c2c457a8f2035fc39cec33a1
Filip <fillip.detterfelt@gmail.com>
2024-05-24 11:29:17 +0200

    Updated

--- src/main/java/com/example/backendpensionat/Controllers/LoginController.java
+++ src/main/java/com/example/backendpensionat/Controllers/LoginController.java
@@ -1,8 +1,6 @@
 package com.example.backendpensionat.Controllers;
 
-import com.example.backendpensionat.Security.EmailTestSender;
 import com.example.backendpensionat.Services.Impl.EmailServiceIMPL;
-import com.example.backendpensionat.Services.Impl.UserServiceIMPL;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
     private final EmailServiceIMPL emailServiceIMPL;
-    private final EmailTestSender emailTestSender;
+
 
     @GetMapping("/login")
     public String login() {
