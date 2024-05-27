cfc61d16b25d67f606a4a00cd262de7248dca6be
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-05-27 14:08:27 +0200

    Testing forgotPassword controllers

--- src/main/java/com/example/backendpensionat/Controllers/LoginController.java
+++ src/main/java/com/example/backendpensionat/Controllers/LoginController.java
@@ -1,17 +1,22 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.Models.User;
 import com.example.backendpensionat.Services.Impl.EmailServiceIMPL;
+import com.example.backendpensionat.Services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @AllArgsConstructor
 @Controller
 public class LoginController {
     private final EmailServiceIMPL emailServiceIMPL;
+    private UserService userService;
 
 
     @GetMapping("/login")
@@ -24,15 +29,17 @@ public class LoginController {
         return "forgotPassword";
     }
 
-    @RequestMapping("/sendForgotPwEmail")
-    public String sendForgotPwEmail() {
-        emailServiceIMPL.sendEmail("filip.exempel@mail.se","Reset your password", "Click on this link to reset");
-        return "forgotPassword";
-    }
+//    @RequestMapping("/sendForgotPwEmail")
+//    public String sendForgotPwEmail(@RequestParam("username") String username, RedirectAttributes rda, Model model) {
+//        User user = userService.findUserByUsername(username);
+//        model.addAttribute("username", username);
+//        String resetURL = "http://localhost:8080/resetPassword?token=123456";
+//        emailServiceIMPL.sendEmail(user.getUsername(), "Reset your password", "Click on this link to reset" + resetURL);
+//        return "redirect:/forgotPassword";
+//    }
 
-
-    @PostMapping("/changePassword")
-    public String changePw(@RequestParam("password") String password) {
-        return "redirect:/changePassword";
+    @GetMapping("/changePassword")
+    public String changePw() {
+        return "changePassword";
     }
 }
