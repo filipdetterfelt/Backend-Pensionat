b03bcdd3bfb5d80966d29cdee4384b156e36a2ab
Robin Blondin <robin.blondin@protonmail.com>
2024-05-28 00:34:07 +0200

    Added controllers related to forgot password

--- src/main/java/com/example/backendpensionat/Controllers/LoginController.java
+++ src/main/java/com/example/backendpensionat/Controllers/LoginController.java
@@ -1,24 +1,33 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.DTO.UserDTO;
+import com.example.backendpensionat.Models.PasswordResetToken;
 import com.example.backendpensionat.Models.User;
+import com.example.backendpensionat.Repos.TokenRepo;
+import com.example.backendpensionat.Repos.UserRepo;
 import com.example.backendpensionat.Services.Impl.EmailServiceIMPL;
+import com.example.backendpensionat.Services.Impl.TokenService;
 import com.example.backendpensionat.Services.Impl.UserServiceIMPL;
-import com.example.backendpensionat.Services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @AllArgsConstructor
 @Controller
 @EnableAsync
 public class LoginController {
     private final EmailServiceIMPL emailServiceIMPL;
+    private final UserRepo userRepo;
     private UserServiceIMPL userService;
+    private final TokenRepo tokenRepo;
+    private TokenService tokenService;
+    private final BCryptPasswordEncoder encoder;
 
     @GetMapping("/login")
     public String login() {
@@ -26,29 +35,49 @@ public class LoginController {
     }
 
     @GetMapping("/forgotPassword")
-    public String forgotPw() {
+    public String forgotPw(Model model) {
+
+        model.addAttribute("user", new UserDTO());
         return "forgotPassword";
     }
 
-    @RequestMapping("/sendForgotPwEmail")
-    public String sendForgotPwEmail(@RequestParam("username") String username, RedirectAttributes rda, Model model) {
-        Optional<User> user = Optional.ofNullable(userService.findUserByUsername(username));
+    @PostMapping("/forgotPassword")
+    public String sendForgotPwEmail(@RequestParam("email") String email) {
+        String output = "";
+        User user = userService.findUserByUsername(email);
+        if (user != null) {
+           output = emailServiceIMPL.sendForgotPasswordEmail(user);
+        }
 
-        if (user.isPresent()) {
-            model.addAttribute("username", username);
-            String resetURL = "http://localhost:8080/changePassword";
-            String emailContent = "Click on link below to reset your password: <br><a href=\"" + resetURL + "\">Reset Password</a>";
-            emailServiceIMPL.sendEmailHTML(username, "Reset your password", emailContent);
-            rda.addFlashAttribute("successfulMessage", true);
-            return "redirect:/forgotPassword";
+        if(output.equals("success")) {
+            return "redirect:/forgotPassword?success";
         } else {
-            rda.addFlashAttribute("errorMessage", true);
-            return "redirect:/forgotPassword";
+            return "redirect:/forgotPassword?error";
         }
     }
 
-    @GetMapping("/changePassword")
-    public String changePw() {
-        return "changePassword";
+    @GetMapping("/resetPassword/{token}")
+    public String resetPassword(@PathVariable String token, Model model) {
+        PasswordResetToken resetToken = tokenRepo.findByToken(token);
+        if(resetToken != null && !emailServiceIMPL.hasExpired(resetToken.getExpiryDate())) {
+            model.addAttribute("email", resetToken.getUser().getUsername());
+            model.addAttribute("token", token);
+            return "resetPassword";
+        }
+        return "redirect:/forgotPassword?expired";
+    }
+
+    @PostMapping("/resetPassword")
+    public String resetPasswordSave(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            @RequestParam("token") String token) {
+        User user = userService.findUserByUsername(email);
+        if(user != null) {
+            user.setPassword(encoder.encode(password));
+            userRepo.save(user);
+            tokenService.deleteByToken(token);
+        }
+        return "redirect:/login";
     }
 }
