9bd6473a8332160e0be9c40cd13eba8a2a3618be
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-05-27 18:40:48 +0200

    Cleaner code

--- src/main/java/com/example/backendpensionat/Controllers/CustomerController.java
+++ src/main/java/com/example/backendpensionat/Controllers/CustomerController.java
@@ -1,11 +1,9 @@
 package com.example.backendpensionat.Controllers;
 
 import com.example.backendpensionat.DTO.CustomerDetailedDTO;
-import com.example.backendpensionat.Models.Customer;
 import com.example.backendpensionat.Services.CustomerService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
