ede9ecb919034ce0e3425c0973c5a4c20a850b0c
Michael Speiner <speiner.m@gmail.com>
2024-05-27 13:07:10 +0200

    Created first controller method for emailTemplateController

--- src/main/java/com/example/backendpensionat/Controllers/emailTemplateController.java
+++ src/main/java/com/example/backendpensionat/Controllers/emailTemplateController.java
@@ -1,9 +1,22 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.DTO.EmailTemplateDTO;
+import com.example.backendpensionat.Repos.EmailTemplateRepo;
+import com.example.backendpensionat.Services.Impl.EmailTemplateServiceIMPL;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @AllArgsConstructor
 @Controller
 public class emailTemplateController {
+    EmailTemplateRepo emailTemplateRepo;
+    EmailTemplateServiceIMPL emailTemplateServiceIMPL;
+
+    @GetMapping("/emailTemplate")
+    public String emailTemplate() {
+        EmailTemplateDTO emailTemplateDTO = emailTemplateServiceIMPL.emailTemplateToDTO(emailTemplateRepo.findById(1L).get());
+        return "emailTemplate";
+    }
+
 }
