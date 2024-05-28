d1f96a827d314421acbbe4789513c20be66851a2
Robin Blondin <robin.blondin@protonmail.com>
2024-05-28 23:59:02 +0200

    added check if customer is blacklisted, or already exists in db

--- src/main/java/com/example/backendpensionat/Controllers/CustomerController.java
+++ src/main/java/com/example/backendpensionat/Controllers/CustomerController.java
@@ -1,6 +1,10 @@
 package com.example.backendpensionat.Controllers;
 
+import com.example.backendpensionat.DTO.BlacklistDetailedDTO;
 import com.example.backendpensionat.DTO.CustomerDetailedDTO;
+import com.example.backendpensionat.Models.Customer;
+import com.example.backendpensionat.Repos.CustomerRepo;
+import com.example.backendpensionat.Services.BlacklistService;
 import com.example.backendpensionat.Services.CustomerService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +12,16 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final CustomerRepo customerRepo;
+    private final BlacklistService blacklistService;
 
 
     @GetMapping("customers")
@@ -38,6 +44,19 @@ public class CustomerController {
 
     @PostMapping("addCustomer")
     public String addCustomers(@ModelAttribute("newCustomers") CustomerDetailedDTO customer) {
+        Optional<Customer> ssn = customerRepo.findCustomerBySsn(customer.getSsn());
+        Optional<Customer> email = customerRepo.findCustomerByEmail(customer.getEmail());
+        BlacklistDetailedDTO blacklist = blacklistService.checkBlackList(customer.getEmail());
+
+        if(blacklist != null && !blacklist.isOk()) {
+            return "redirect:/addNewCustomer?wasBlackListed";
+        }
+
+        if(ssn.isPresent()) {
+            return "redirect:/addNewCustomer?ssnExist";
+        } else if(email.isPresent()) {
+            return "redirect:/addNewCustomer?emailExist";
+        }
 
         customer.setBookings(new ArrayList<>());
         customerService.addCustomer(customer);
